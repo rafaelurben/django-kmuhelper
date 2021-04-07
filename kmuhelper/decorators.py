ed83e8b502b52e0ea464724b8a83307cb2501b70
rafaelurben <rafael.urben@gmail.com>
2021-04-07 18:44:10 +0200

    Added confirm_action decorator

---
+++ kmuhelper/decorators.py
@@ -0,0 +1,17 @@
+from functools import wraps
+
+from django.http import JsonResponse
+from django.urls import reverse
+from django.shortcuts import redirect, render
+
+
+def confirm_action(action_message):
+    def decorator(function):
+        @wraps(function)
+        def wrap(request, *args, **kwargs):
+            if request.method == "POST":
+                return function(request, *args, **kwargs)
+            else:
+                return render(request, "admin/kmuhelper/_confirm.html", {"action": action_message})
+        return wrap
+    return decorator
--- kmuhelper/main/views.py
+++ kmuhelper/main/views.py
@@ -7,6 +7,7 @@ from django.views.decorators.cache import never_cache
 from django.views.decorators.csrf import csrf_exempt
 
 from kmuhelper.main.models import Einstellung, Geheime_Einstellung, Produkt, Kunde, Kategorie, Lieferant, Lieferung, Bestellung, Bestellungsposten
+from kmuhelper.decorators import confirm_action
 
 ###############
 
@@ -14,49 +15,42 @@ from kmuhelper.main.models import Einstellung, Geheime_Einstellung, Produkt, Kun
 
 
 @login_required(login_url=reverse_lazy("admin:login"))
+@confirm_action("Lieferant zuordnen")
 def lieferant_zuordnen(request, object_id):
-    if request.method == "POST":
-        if Lieferant.objects.filter(id=int(object_id)).exists():
-            count = Lieferant.objects.get(id=int(object_id)).zuordnen()
-            messages.success(request, ('Lieferant wurde ' + (('{} neuen Produkten' if count > 1 else 'einem neuen Produkt')
-                                                            if count != 0 else "keinem neuen Produkt") + ' zugeordnet!').format(count))
-        else:
-            messages.error(request, "Lieferant konnte nicht gefunden werden!")
-        return redirect(reverse("admin:kmuhelper_lieferant_change", args=[object_id]))
+    if Lieferant.objects.filter(id=int(object_id)).exists():
+        count = Lieferant.objects.get(id=int(object_id)).zuordnen()
+        messages.success(request, ('Lieferant wurde ' + (('{} neuen Produkten' if count > 1 else 'einem neuen Produkt')
+                                                        if count != 0 else "keinem neuen Produkt") + ' zugeordnet!').format(count))
     else:
-        return render(request, "admin/kmuhelper/_confirm.html", {"action": "Lieferant zuordnen"})
+        messages.error(request, "Lieferant konnte nicht gefunden werden!")
+    return redirect(reverse("admin:kmuhelper_lieferant_change", args=[object_id]))
 
 @login_required(login_url=reverse_lazy("admin:login"))
+@confirm_action("Lieferung einlagern")
 def lieferung_einlagern(request, object_id):
-    if request.method == "POST":
-        if Lieferung.objects.filter(id=int(object_id)).exists():
-            if Lieferung.objects.get(id=int(object_id)).einlagern():
-                messages.success(request, "Lieferung eingelagert!")
-            else:
-                messages.error(
-                    request, "Lieferung konnte nicht eingelagert werden!")
+    if Lieferung.objects.filter(id=int(object_id)).exists():
+        if Lieferung.objects.get(id=int(object_id)).einlagern():
+            messages.success(request, "Lieferung eingelagert!")
         else:
-            messages.error(request, "Lieferung konnte nicht gefunden werden!")
-        return redirect(reverse("admin:kmuhelper_lieferung_change", args=[object_id]))
+            messages.error(
+                request, "Lieferung konnte nicht eingelagert werden!")
     else:
-        return render(request, "admin/kmuhelper/_confirm.html", {"action": "Lieferung einlagern"})
+        messages.error(request, "Lieferung konnte nicht gefunden werden!")
+    return redirect(reverse("admin:kmuhelper_lieferung_change", args=[object_id]))
 
 
 @login_required(login_url=reverse_lazy("admin:login"))
+@confirm_action("Registrierungsmail versenden")
 def kunde_email_registriert(request, object_id):
-    if request.method == "POST":
-        if Kunde.objects.filter(id=int(object_id)).exists():
-            if Kunde.objects.get(id=int(object_id)).send_register_mail():
-                messages.success(request, "Registrierungsmail gesendet!")
-            else:
-                messages.error(
-                    request, "Registrierungsmail konnte nicht gesendet werden!")
+    if Kunde.objects.filter(id=int(object_id)).exists():
+        if Kunde.objects.get(id=int(object_id)).send_register_mail():
+            messages.success(request, "Registrierungsmail gesendet!")
         else:
-            messages.error(request, "Kunde konnte nicht gefunden werden!")
-        return redirect(reverse("admin:kmuhelper_kunde_change", args=[object_id]))
+            messages.error(
+                request, "Registrierungsmail konnte nicht gesendet werden!")
     else:
-        return render(request, "admin/kmuhelper/_confirm.html", {"action": "Registrierungsmail versenden"})
-
+        messages.error(request, "Kunde konnte nicht gefunden werden!")
+    return redirect(reverse("admin:kmuhelper_kunde_change", args=[object_id]))
 
 @login_required(login_url=reverse_lazy("admin:login"))
 def bestellung_pdf_ansehen(request, object_id):
@@ -72,47 +66,42 @@ def bestellung_pdf_ansehen(request, object_id):
 
 
 @login_required(login_url=reverse_lazy("admin:login"))
+@confirm_action("Rechnung an Kunden per E-Mail senden")
 def bestellung_pdf_an_kunden_senden(request, object_id):
-    if request.method == "POST":
-        if Bestellung.objects.filter(id=int(object_id)).exists():
-            if Bestellung.objects.get(id=int(object_id)).send_pdf_rechnung_to_customer():
-                messages.success(request, "Rechnung an Kunden gesendet!")
-            else:
-                messages.error(
-                    request, "Rechnung konnte nicht an Kunden gesendet werden!")
+    if Bestellung.objects.filter(id=int(object_id)).exists():
+        if Bestellung.objects.get(id=int(object_id)).send_pdf_rechnung_to_customer():
+            messages.success(request, "Rechnung an Kunden gesendet!")
         else:
-            messages.error(request, "Bestellung konnte nicht gefunden werden!")
-        return redirect(reverse("admin:kmuhelper_bestellung_change", args=[object_id]))
+            messages.error(
+                request, "Rechnung konnte nicht an Kunden gesendet werden!")
     else:
-        return render(request, "admin/kmuhelper/_confirm.html", {"action": "Rechnung an Kunden senden"})
+        messages.error(request, "Bestellung konnte nicht gefunden werden!")
+    return redirect(reverse("admin:kmuhelper_bestellung_change", args=[object_id]))
 
 
 @login_required(login_url=reverse_lazy("admin:login"))
+@confirm_action("Bestellung duplizieren")
 def bestellung_duplizieren(request, object_id):
-    if request.method == "POST":
-        if Bestellung.objects.filter(id=int(object_id)).exists():
-            new = Bestellung.objects.get(id=int(object_id)).duplicate()
-            messages.success(request, "Bestellung wurde dupliziert! HINWEIS: Dies ist die neu erstellte Bestellung!")
-            return redirect(reverse("admin:kmuhelper_bestellung_change", args=[new.pk]))
-        else:
-            messages.error(request, "Bestellung konnte nicht gefunden werden!")
-        return redirect(reverse("admin:kmuhelper_bestellung_change", args=[object_id]))
+    if Bestellung.objects.filter(id=int(object_id)).exists():
+        new = Bestellung.objects.get(id=int(object_id)).duplicate()
+        messages.success(request, "Bestellung wurde dupliziert! HINWEIS: Dies ist die neu erstellte Bestellung!")
+        return redirect(reverse("admin:kmuhelper_bestellung_change", args=[new.pk]))
     else:
-        return render(request, "admin/kmuhelper/_confirm.html", {"action": "Bestellung duplizieren"})
+        messages.error(request, "Bestellung konnte nicht gefunden werden!")
+    return redirect(reverse("admin:kmuhelper_bestellung_change", args=[object_id]))
 
 
 @login_required(login_url=reverse_lazy("admin:login"))
+@confirm_action("Bestellung zu Lieferung kopieren")
 def bestellung_zu_lieferung(request, object_id):
-    if request.method == "POST":
-        if Bestellung.objects.filter(id=int(object_id)).exists():
-            new = Bestellung.objects.get(id=int(object_id)).zu_lieferung()
-            messages.success(request, "Bestellung wurde zu einer Lieferung kopiert!")
-            return redirect(reverse("admin:kmuhelper_lieferung_change", args=[new.pk]))
-        else:
-            messages.error(request, "Bestellung konnte nicht gefunden werden!")
-        return redirect(reverse("admin:kmuhelper_bestellung_change", args=[object_id]))
+    if Bestellung.objects.filter(id=int(object_id)).exists():
+        new = Bestellung.objects.get(id=int(object_id)).zu_lieferung()
+        messages.success(request, "Bestellung wurde zu einer Lieferung kopiert!")
+        return redirect(reverse("admin:kmuhelper_lieferung_change", args=[new.pk]))
     else:
-        return render(request, "admin/kmuhelper/_confirm.html", {"action": "Bestellung zu Lieferung kopieren"})
+        messages.error(request, "Bestellung konnte nicht gefunden werden!")
+    return redirect(reverse("admin:kmuhelper_bestellung_change", args=[object_id]))
+
 
 # Kunden-Entpunkte
 
