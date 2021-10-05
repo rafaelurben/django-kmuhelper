9ace75c24afeaccdcc711553ab509d8232cd291a
rafaelurben <rafael.urben@gmail.com>
2021-10-05 23:02:59 +0200

    Updated startup.py

--- kmuhelper/startup.py
+++ kmuhelper/startup.py
@@ -5,28 +5,30 @@ from kmuhelper.main.models import Einstellung, Geheime_Einstellung
 
 
 def log(string, *args):
-    print("[deep_pink4][KMUHelper][/] -", string, *args)
+    print("[deep_pink4][KMUHelper startup.py][/] -", string, *args)
 
 # Einstellungen festlegen
 
 
-log("startup.py running...")
-
-log("startup.py DEBUG is", settings.DEBUG)
+log("Started... DEBUG is", settings.DEBUG)
 
 try:
     Einstellung.objects.update_or_create(
         id="wc-url", defaults={
-            "typ": "url", "name": "WooCommerce Shop-Url"})
+            "typ": "url",
+            "name": "WooCommerce Shop-Url"})
     Einstellung.objects.update_or_create(
         id="email-stock-warning-receiver", defaults={
-            "typ": "email", "name": "E-Mail für Warnungen zum Lagerbestand"})
+            "typ": "email",
+            "name": "E-Mail für Warnungen zum Lagerbestand"})
     Einstellung.objects.update_or_create(
         id="email-show-buttons", defaults={
-            "typ": "bool", "name": "E-Mail Knöpfe anzeigen"})
+            "typ": "bool",
+            "name": "E-Mail-Knöpfe anzeigen"})
     Einstellung.objects.update_or_create(
         id="email-signature", defaults={
-            "typ": "text", "name": "E-Mail Signatur"})
+            "typ": "text",
+            "name": "E-Mail-Signatur"})
 
     Geheime_Einstellung.objects.update_or_create(
         id="wc-consumer_key", defaults={
@@ -38,6 +40,6 @@ try:
         id="wc-url", defaults={
             "typ": "url"})
 except Exception as error:
-    log("startup.py failed: Error while adding settings:", type(error), error)
+    log("Failed: Error while adding/updating settings:", type(error), error)
 
-log("startup.py ended")
+log("Ended.")
