40481bd1bfec9bfb165413fdf048596216d56333
rafaelurben <github@rafaelurben.ch>
2023-03-11 17:57:04 +0100

    (#203) Included view permission in change permission

--- kmuhelper/modules/config.py
+++ kmuhelper/modules/config.py
@@ -117,7 +117,10 @@ def get_module_home_context(request, module_name):
                         'delete': request.user.has_perm(f"kmuhelper.delete_{model._meta.model_name}"),
                         'view': request.user.has_perm(f"kmuhelper.view_{model._meta.model_name}")},
                     'view_only': not request.user.has_perm(f"kmuhelper.change_{model._meta.model_name}"),
-                } for model in get_models(module.get('model_names')) if request.user.has_perm(f"kmuhelper.view_{model._meta.model_name}")
+                } for model in get_models(module.get('model_names')) if (
+                    request.user.has_perm(f"kmuhelper.view_{model._meta.model_name}") or
+                    request.user.has_perm(f"kmuhelper.change_{model._meta.model_name}")
+                )
             ],
             'name': module.get('title'),
         }],
@@ -131,6 +134,7 @@ def get_module_home_context(request, module_name):
 def user_has_module_permission(user, module_name):
     module = MODULES.get(module_name)
     for model_name in module.get('model_names'):
-        if user.has_perm(f"kmuhelper.view_{model_name.lower()}"):
+        name = model_name.lower()
+        if user.has_perm(f"kmuhelper.view_{name}") or user.has_perm(f"kmuhelper.change_{name}"):
             return True
     return False
