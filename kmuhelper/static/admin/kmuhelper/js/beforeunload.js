d6b582c022cd94552a0582521b3de962f030b77a
rafaelurben <github@rafaelurben.ch>
2024-04-14 15:32:40 +0200

    Fixed code style issues

--- kmuhelper/static/admin/kmuhelper/js/beforeunload.js
+++ kmuhelper/static/admin/kmuhelper/js/beforeunload.js
@@ -1,28 +1,33 @@
+/**
+ * Helper method that warns a user if they try to leave a page on which the have changed form values.
+ */
+
 window.addEventListener("load", function () {
     if (typeof django !== 'undefined') {
-        var $ = django.jQuery;
+        const $ = django.jQuery;
     } else if (typeof jQuery !== 'undefined') {
-        var $ = jQuery;
+        const $ = jQuery;
     } else if (typeof $ === 'undefined') {
         console.error("jQuery not found!");
         return;
     }
 
     window.initialData = "";
-    var isSubmitting = false;
+    let isSubmitting = false;
 
-    $('form').not("#changelist-search").submit(function (event) {
+    const $form = $('form');
+    $form.not("#changelist-search").submit(() => {
         isSubmitting = true;
         console.log("Form submitted...");
     })
 
-    window.initialData = $('form').not("#changelist-search").serialize();
+    window.initialData = $form.not("#changelist-search").serialize();
 
     $(window).on('beforeunload', function () {
         console.log("[Beforeunload] - Event triggered!");
-        var formChanged = $('form').not("#changelist-search").serialize() != window.initialData;
-        console.log("[Beforeunload] - Form changed:", formChanged);
-        if (!isSubmitting && formChanged) {
+        const formDidChange = $('form').not("#changelist-search").serialize() !== window.initialData;
+        console.log("[Beforeunload] - Form changed:", formDidChange);
+        if (!isSubmitting && formDidChange) {
             console.log("[Beforeunload] - Unload prevented!");
             return 'Du hast ungesicherte Änderungen vorgenommen. Wenn du die Seite verlässt, werden diese gelöscht.'
         }
